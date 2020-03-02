cfaa5d40abd1e76dfaa5276dd9a44637f346adea
helloferdie <ferdie_int@yahoo.com>
2020-03-02 13:20:12 +0700

    Product Details

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
+import ItemDetail from './ItemDetail';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
@@ -10,9 +11,12 @@ function App() {
     <Router>
       <div className="App">
         <Nav />
-        <Route path="/" exact component={Home}/>
-        <Route path="/about" exact component={About}/>
-        <Route path="/shop" exact component={Shop}/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" exact component={About}/>
+          <Route path="/shop" exact component={Shop}/>
+          <Route path="/shop/:id" component={ItemDetail}/>
+        </Switch>
       </div>
     </Router>
   );
--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -2,41 +2,43 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import {Link} from 'react-router-dom';
 
-function Shop() {
+function ItemDetail({ match }) {
   useEffect(() => {
-    fetchItems();
+    fetchItem();
   }, []);
 
-  const [items, setItems] = useState([]);
+  const [item, setItem] = useState([]);
 
-  const fetchItems = async () => {
+  const fetchItem = async () => {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOjIsImV4cCI6MTU4MzcyNDA5MCwidG9rZW5faWQiOiJDNkdRU1pUMlY0cFdaVDJJMXFkcE1EMDVDOVF4ckRJdE1UNE5COGhlZmVhbExTUENxYiIsInVzZXJfaWQiOjF9.VVslVLvidgGJfh8E89PT6mZ8e3hnH4jjtTGg1CE9rnY");
 
-    var raw = JSON.stringify({"page":1,"items_per_page":15,"order_by_field":"","order_by_direction":"","product_no":"","product_name":"","description_1":"","description_2":"","brand_id":0,"category_id":0,"all_outlets":"","is_active":"1","show_relationship":true});
+    var raw = JSON.stringify({"uuid": match.params.id});
     var requestOptions = {
       method: 'POST',
       headers: myHeaders,
-      body: raw
+      body: raw,
+      redirect: 'follow'
     };
 
-    fetch("https://apidev.shushu.co/products", requestOptions)
+    fetch("https://apidev.shushu.co/products/view_by_uuid", requestOptions)
       .then(response => response.json())
-      .then(result => setItems(result.data.items))
+      .then(function(result){
+        setItem(result.data);
+      })
       .catch(error => console.log('error', error));
-
-      //console.log(items)
   }
 
 
   return (
     <div>
-      {items.map(item => (
-          <h1 key={item.id}><Link to={`/shop/${item.uuid}`}>{item.product_name}</Link></h1>
-      ))}
+  <h1>{item.product_no} - {item.product_name}</h1>
+  <p>{item.description_1}</p>
+  <p>{item.description_2}</p>
+  <img src={item.photo_1_url} />
     </div>
   );
 }
 
-export default Shop;
+export default ItemDetail;
